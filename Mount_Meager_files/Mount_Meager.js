c3cce4dd3ae947712895e4e97b7a820bee9fd27a
Kate <allstadt@uw.edu>
2014-09-23 17:23:30 -0700

    Added Oso and one other thing

--- Mount_Meager_files/Mount_Meager.js
+++ Mount_Meager_files/Mount_Meager.js
@@ -1,9 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20130923
+// Created by iWeb 3.0.4 local-build-20140729
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,302),url:'Mount_Meager_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'Mount_Meager_files/stroke_1.png'},{rect:new IWRect(5,-5,309,10),url:'Mount_Meager_files/stroke_2.png'},{rect:new IWRect(314,-5,10,10),url:'Mount_Meager_files/stroke_3.png'},{rect:new IWRect(314,5,10,302),url:'Mount_Meager_files/stroke_4.png'},{rect:new IWRect(314,307,10,10),url:'Mount_Meager_files/stroke_5.png'},{rect:new IWRect(5,307,309,10),url:'Mount_Meager_files/stroke_6.png'},{rect:new IWRect(-5,307,10,10),url:'Mount_Meager_files/stroke_7.png'}],new IWSize(319,312))});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Mount_Meager_files/Mount_MeagerMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
--- Welcome_files/Welcome.js
+++ Welcome_files/Welcome.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20130923
+// Created by iWeb 3.0.4 local-build-20140729
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,272),url:'Welcome_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'Welcome_files/stroke_1.png'},{rect:new IWRect(5,-5,313,10),url:'Welcome_files/stroke_2.png'},{rect:new IWRect(318,-5,10,10),url:'Welcome_files/stroke_3.png'},{rect:new IWRect(318,5,10,272),url:'Welcome_files/stroke_4.png'},{rect:new IWRect(318,277,10,10),url:'Welcome_files/stroke_5.png'},{rect:new IWRect(5,277,313,10),url:'Welcome_files/stroke_6.png'},{rect:new IWRect(-5,277,10,10),url:'Welcome_files/stroke_7.png'}],new IWSize(323,282)),stroke_1:new IWStrokeParts([{rect:new IWRect(-5,5,10,147),url:'Welcome_files/stroke_8.png'},{rect:new IWRect(-5,-5,10,10),url:'Welcome_files/stroke_9.png'},{rect:new IWRect(5,-5,324,10),url:'Welcome_files/stroke_10.png'},{rect:new IWRect(329,-5,10,10),url:'Welcome_files/stroke_11.png'},{rect:new IWRect(329,5,10,147),url:'Welcome_files/stroke_12.png'},{rect:new IWRect(329,152,10,10),url:'Welcome_files/stroke_13.png'},{rect:new IWRect(5,152,324,10),url:'Welcome_files/stroke_14.png'},{rect:new IWRect(-5,152,10,10),url:'Welcome_files/stroke_15.png'}],new IWSize(334,157)),stroke_2:new IWStrokeParts([{rect:new IWRect(-5,5,10,104),url:'Welcome_files/stroke_16.png'},{rect:new IWRect(-5,-5,10,10),url:'Welcome_files/stroke_17.png'},{rect:new IWRect(5,-5,324,10),url:'Welcome_files/stroke_18.png'},{rect:new IWRect(329,-5,10,10),url:'Welcome_files/stroke_19.png'},{rect:new IWRect(329,5,10,104),url:'Welcome_files/stroke_20.png'},{rect:new IWRect(329,109,10,10),url:'Welcome_files/stroke_21.png'},{rect:new IWRect(5,109,324,10),url:'Welcome_files/stroke_22.png'},{rect:new IWRect(-5,109,10,10),url:'Welcome_files/stroke_23.png'}],new IWSize(334,114))});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
